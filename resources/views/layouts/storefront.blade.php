f2591aeb44eb2d339ea7b5be18438f824685494f
Seamus Connolly <seamusconnolly@seamuss-mbp.lan>
2019-12-11 20:09:40 -0500

    removed google analytics

--- resources/views/layouts/storefront.blade.php
+++ resources/views/layouts/storefront.blade.php
@@ -2,15 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152610947-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-152610947-1');
-    </script>
+    
 
 
     <meta charset="utf-8">
